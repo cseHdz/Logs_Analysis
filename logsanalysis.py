306125f1fe2cd582f029a4ea6c76db0286014df8
Carlos Hernandez <carlos.hdz@me.com>
2018-01-24 16:19:32 -0500

    Files are now PEP8 Compliant

--- logsanalysis.py
+++ logsanalysis.py
@@ -1,15 +1,17 @@
 
-#!/usr/bin/env python 2.7
+# !/usr/bin/env python 2.7
 
-#Logs analysis
+# Logs analysis
 
 from logsdb import get_popular_articles, get_popular_authors, get_errors
 import psycopg2
+
 
 def main():
     articles_analysis()
     authors_analysis()
     errors_analysis()
+
 
 def articles_analysis():
     print ("\nMost popular articles:\n")
@@ -18,6 +20,7 @@ def articles_analysis():
     for record in results:
         print '"{}"'.format(record[0]) + " - " + str(record[1])
 
+
 def authors_analysis():
     print ("\nMost popular authors:\n")
     results = get_popular_authors()
@@ -25,11 +28,14 @@ def authors_analysis():
     for record in results:
         print record[0] + " - " + str(record[1])
 
+
 def errors_analysis():
     print ("\nDays with more than 1% errors:\n")
     results = get_errors()
 
     for record in results:
-        print "{:%b %d, %Y}".format(record[0]) + " - " + "{0:.1f}%".format(record[1]*100) + " errors"
+        print ("{:%b %d, %Y}".format(record[0]) + " - " +
+               "{0:.1f}%".format(record[1]*100) + " errors")
+
 
 main()
--- logsdb.py
+++ logsdb.py
@@ -1,18 +1,21 @@
 
-#!/usr/bin/env python 2.7.12
+# !/usr/bin/env python 2.7.12
 
 import psycopg2
 
 DBNAME = "news"
 
+
 def get_popular_articles():
     """Return the most popular articles, sorted by number of views"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("Select title, count from article_popularity order by count desc")
+    c.execute("Select title, count from article_popularity" +
+              "order by count desc")
     results = c.fetchall()
     db.close()
     return results
+
 
 def get_popular_authors():
     """Return the most popular authors, sorted by number of views"""
@@ -23,11 +26,13 @@ def get_popular_authors():
     db.close()
     return results
 
+
 def get_errors():
     """Return all days with more than 1 percernt error ocurrences"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("Select date, percent_total from request_proportions where status = '404 NOT FOUND' and percent_total > 0.01")
+    c.execute("Select date, percent_total from request_proportions " +
+              "where status = '404 NOT FOUND' and percent_total > 0.01")
     results = c.fetchall()
     db.close()
     return results
